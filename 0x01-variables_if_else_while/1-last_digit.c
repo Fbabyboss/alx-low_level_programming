5e212ead0634133dc27b44245cc9f63419b49761
fbabyboss <itsraydanielz@gmail.com>
2023-02-17 16:14:33 -0800

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,11 +16,11 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 5)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else if (n < 5)
+	else if (n < 6)
 	{
 		printf("%d is negative\n", n);
 	}
