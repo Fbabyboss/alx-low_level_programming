1d5645741cd059ec138f51d4da9f1549cf8aeed3
fbabyboss <itsraydanielz@gmail.com>
2023-02-22 09:35:50 +0100

    C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,16 +7,12 @@
  */
 int main(void)
 {
-    print_times_table(2);
+    print_times_table(3);
     _putchar('\n');
-    print_times_table(10);
+    print_times_table(5);
     _putchar('\n');
-    print_times_table(13);
+    print_times_table(98);
     _putchar('\n');
-    print_times_table(-2);
-    _putchar('\n');
-    print_times_table(0);
-    _putchar('\n');
-    print_times_table(100);
+    print_times_table(12);  
     return (0);
 }
